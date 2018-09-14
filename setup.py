55ecaecd6e83155592e9d84fe2aefd647d2aca07
Bernardo García <botibagl@gmail.com>
2018-09-14 16:15:05 -0500

    Cookiecutter Datascience project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML techniques implementation to predict the bunch of bananas at Uraba region',
+    author='Bernardo Augusto García Loaiza',
+    license='',
+)
